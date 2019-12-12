3c02ac6d54231c54d7f359973de48dc00c83a2f9
root <root@localhost.localdomain>
2019-12-12 15:14:49 +0000

    add vars

---
+++ variable.tf
@@ -0,0 +1,24 @@
+variable "project" {
+  description = "GCP project name"
+  default     = "tomh-lab-1000"
+}
+
+variable "zone" {
+  description = "GCP zone"
+  default     = "europe-west2-c"
+}
+
+variable "region" {
+  description = "GCP region"
+  default     = "europe-west2"
+}
+
+variable "publicnetwork" {
+ description = "network for public webserver"
+ default = "staging_public"
+}
+
+variable "privatenetwork" {
+ description = "network for private webserver"
+ default = "staging_private"
+}
